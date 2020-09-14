7e374b6b64c32a43b977f82fdea9c36161aa8111
Fiona Gridley <fionagridley@Fionas-MacBook-Pro.local>
2020-09-14 15:04:44 -0400

    fix makefile

--- B/Other/xyes.ts
+++ B/Other/xyes.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 const args = process.argv.slice(2)
 
-const printArgs = (limit?: number, args: String[] = ["hello world"]): void => {
-    for ( var i = 0; i < limit; i++ ) {
-        console.log(args.join(" "));
-    }
-} 
+// const printArgs = (limit?: number, args: String[] = ["hello world"]): void => {
+//     for ( var i = 0; i < limit; i++ ) {
+//         console.log(args.join(" "));
+//     }
+// } 
 
 if (!args.length) {
     console.log('Hello world')
 } else if (args[0] === '-limit') {
     console.log(`limit: ${args.slice(1).join(" ")}`)
-    printArgs(20)
+    // printArgs(20)
 } else {
     console.log(`no limit: ${args.join(" ")}`)
 }
