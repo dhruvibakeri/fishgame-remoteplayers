db990676a617a284a505dd682be25d90b223d3f5
Andrew Leung <andrewleung104@gmail.com>
2020-09-22 15:37:19 -0400

    use jsonparse to simplify json parsing

---
+++ E/Other/src/parseUtils.ts
@@ -0,0 +1,25 @@
+const Parser = require("jsonparse");
+
+/**
+ * Takes in a string containing a series of "well formatted" JSON values and
+ * returns an array of single JSON values in string format.
+ *
+ * @param jsonString The string which is a series of "well formatted" JSON values.
+ * @return The parsed array of separate JSON value strings.
+ */
+const parseJsonSequence = (jsonString: String): Array<String> => {
+  const p = new Parser();
+  let parsedValues: Array<String> = [];
+
+  p.onValue = (value: string) => {
+    if (p.stack.length === 0) {
+      parsedValues.push(value);
+    }
+  };
+
+  p.write(jsonString);
+
+  return parsedValues;
+};
+
+export default parseJsonSequence;
