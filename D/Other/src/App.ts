fbbef8997a8a4c46ba746df57043d733ef013cbc
Andrew Leung <andrewleung104@gmail.com>
2020-09-21 22:42:32 -0400

    add tests and clean app.ts

--- D/Other/src/App.ts
+++ D/Other/src/App.ts
@@ -1,27 +1,32 @@
 import express from "express";
 import open from "open";
+import { fetchSizeFromArgs } from "./utils";
+
 const app = express();
 const port = 8080; // default port to listen
+const usageMessage = "usage: ./xgui positive-integer";
+
 app.set("view engine", "pug");
+app.use(express.static("build"));
 
-const size = process.argv[2];
-const sizeNum = size && parseFloat(size);
-
-if (sizeNum && sizeNum > 0) {
-  // define a route handler for the default home page
-  app.get("/", (req, res) => {
-    res.render("hexagonView", {
-      size: sizeNum,
-    });
-  });
-  // start the Express server
-  const server = app.listen(port, async () => {
-    console.log(`server started at http://localhost:${port}`);
-    open(`http://localhost:${port}`, {app: 'google-chrome'});
-  });
-  app.post("/close", (req, res) => {
-    process.exit();
-  });
-} else {
-  console.log("usage: ./xgui positive-integer");
+let sizeNum: number;
+try {
+  sizeNum = fetchSizeFromArgs(process.argv);
+} catch {
+  console.log(usageMessage);
 }
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.render("hexagonView", {
+    size: sizeNum,
+  });
+});
+// start the Express server
+const server = app.listen(port, async () => {
+  console.log(`server started at http://localhost:${port}`);
+  open(`http://localhost:${port}`, { app: "google-chrome" });
+});
+app.post("/close", (req, res) => {
+  process.exit();
+});
