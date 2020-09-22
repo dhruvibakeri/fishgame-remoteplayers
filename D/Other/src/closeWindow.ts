2735d51fcacd2b686df21597cb2e242ded119b71
Fiona Gridley <fgridley@vdi-linux-052.ccs.neu.edu>
2020-09-21 22:10:35 -0400

    separate js from view

--- D/Other/src/closeWindow.ts
+++ D/Other/src/closeWindow.ts
@@ -1,11 +1,10 @@
-import { EventEmitter } from "events"
-const emitter = new EventEmitter();
-
-const closeWindow = (): void => {
-    console.log('window closing');
-    emitter.emit('closed');
-    // window.close();
+function closeWindow() {
+  window.dispatchEvent(new Event('unload'));
+  window.close();
 }
 
-const hexagon = document.getElementById('hexagon')
-hexagon.addEventListener("click", (e: MouseEvent) => closeWindow());
+const hexagon = document.getElementById('hexagon').onclick = (e: MouseEvent) => closeWindow();
+
+window.addEventListener("unload", () => {
+  navigator.sendBeacon("/close", "");
+});
